9202278ed338bef5b8efbc12df4c0192ef329cce
ctrellet <christophe@trellet.ch>
2017-03-01 16:40:23 +0100

    Project after stream implementation

--- src/main/java/streamoperations/StreamOperations.java
+++ src/main/java/streamoperations/StreamOperations.java
@@ -5,6 +5,7 @@ package streamoperations;
 
         import java.util.ArrayList;
         import java.util.List;
+        import java.util.stream.Collectors;
 
 public class StreamOperations {
 
@@ -15,47 +16,58 @@ public class StreamOperations {
     }
 
     public long countPersonsWithAge(int age) {
-        long count = 0;
-        for (Person person : persons) {
-            if (person.getAge() == age) {
-                count++;
-            }
-        }
-        return count;
+//        long count = 0;
+//        for (Person person : persons) {
+//            if (person.getAge() == age) {
+//                count++;
+//            }
+//        }
+
+        ;
+        return persons.stream().filter(p -> p.getAge()==age).count();
     }
 
     public long countMarriedPeople() {
-        long count = 0;
-        for (Person person : persons) {
-            if (person.isMarried()) {
-                count++;
-            }
-        }
-        return count;
+//        long count = 0;
+//        for (Person person : persons) {
+//            if (person.isMarried()) {
+//                count++;
+//            }
+//        }
+        return persons.stream().filter(p -> p.isMarried()).count();
     }
 
     public List<Person> filterByLanguage(String language) {
-        ArrayList<Person> persons = new ArrayList<>();
-        for (Person person : this.persons) {
-            if (person.getLanguage().equals(language)) {
-                persons.add(person);
-            }
-        }
-        return persons;
+//        ArrayList<Person> persons = new ArrayList<>();
+//        for (Person person : this.persons) {
+//            if (person.getLanguage().equals(language)) {
+//                persons.add(person);
+//            }
+//        }
+        return persons.stream().filter(p -> p.getLanguage().equals(language)).collect(Collectors.toList());
     }
 
     public List<Person> toUpperCaseAndSorted() {
-        ArrayList<Person> persons = new ArrayList<>();
-        for (Person person : this.persons) {
-            Person personOther = new Person();
-            personOther.setLanguage(person.getLanguage().toUpperCase());
-            personOther.setName(person.getName().toUpperCase());
-            personOther.setSurname(person.getSurname().toUpperCase());
-            personOther.setMarried(person.isMarried());
-            persons.add(personOther);
-        }
-        return persons;
+//        ArrayList<Person> persons = new ArrayList<>();
+//        for (Person person : this.persons) {
+//            Person personOther = new Person();
+//            personOther.setLanguage(person.getLanguage().toUpperCase());
+//            personOther.setName(person.getName().toUpperCase());
+//            personOther.setSurname(person.getSurname().toUpperCase());
+//            personOther.setMarried(person.isMarried());
+//            persons.add(personOther);
+//        }
+//        return persons;
+        return persons.stream().map(per -> per = copyAndUpperCase(per)).collect(Collectors.toList());
+    }
 
+    private Person copyAndUpperCase(Person person){
+        Person personOther = new Person();
+        personOther.setLanguage(person.getLanguage().toUpperCase());
+        personOther.setName(person.getName().toUpperCase());
+        personOther.setSurname(person.getSurname().toUpperCase());
+        personOther.setMarried(person.isMarried());
+        return personOther;
     }
 
 
